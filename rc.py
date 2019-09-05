91ba8c0d598f81e4c09f451a87dd218d3e8360d1
nprav <7060774+nprav@users.noreply.github.com>
2019-09-04 20:32:25 -0400

    Incremental updates to rc.py and test.py.
    - Added basic doc strings to rc.py and test.py.
    - rc.py  can now plot an input section
    - test.py can now test rc.py by dodging plt.show() with unittest.mock.patch.

--- rc.py
+++ rc.py
@@ -22,24 +22,55 @@ from scipy.optimize import minimize
 class RcSection():
     """General concrete section class.
     """
+    rebar_details = ['x', 'y', 'D']
+    rectangle_kwargs = {'hatch': '/', 'color': 'b'}
+    circle_kwargs = {'color': 'k'}
 
-    def __init__(self, width, thk):
+    def __init__(self, width=200, thk=200):
+        """Instantiate concrete section with a width (x) and thickness (y).
+        """
         self.width = width
         self.thk = thk
-        self.rebars = []
+        self.rebars = pd.DataFrame(columns=RcSection.rebar_details)
+        self.num_rebars = len(self.rebars)
 
     def get_extents(self):
+        """Return the boundaries of the defined section.
+        """
         print("Section Size : ({}, {})".format(
                 self.width, self.thk))
         return self.width, self.thk
 
+    def add_rebar(self, D=10, x=0, y=175):
+        """Add a single rebar to the section, defined by diameter,
+        x position, and y position.
+        """
+        self.rebars.loc[self.num_rebars] = [x, y, D]
+        self.num_rebars = len(self.rebars)
+        print(
+            "Rebar added; pos = ({}, {}), od = {}".format(
+                *self.rebars.iloc[-1]
+                )
+            )
+
     def plot(self):
-        xy = (-self.width/2, 0)
-        rectangle = plt.Rectangle(xy, self.width, self.thk)
+        """Plot the defined section.
+        """
         fig, axis = plt.subplots()
+        xy = (-self.width/2, 0)
+        rectangle = plt.Rectangle(xy, self.width, self.thk,
+                                  **RcSection.rectangle_kwargs)
         axis.add_patch(rectangle)
+
+        for (x, y, D) in self.rebars.values:
+            circle = plt.Circle((x, y), D/2, **RcSection.circle_kwargs)
+            axis.add_patch(circle)
+
         axis.set_xlim((-self.width/2*1.1, self.width/2*1.1))
         axis.set_ylim((-self.thk/2*0.1, self.thk*1.05))
+        plt.axis('equal')
+        plt.show()
+
         return fig, axis
 
 
@@ -59,11 +90,4 @@ class WBeam(RcSection):
 
 
 class CustomBeam(RcSection):
-    pass
-
-
-# %% Define reinforcement class
-
-
-class Rebar():
     pass
--- test.py
+++ test.py
@@ -10,34 +10,56 @@ Script for testing other scripts in the module.
 # %% Import Necessary Modules
 
 import unittest
-from rc import RcSection, Slab, RectangularBeam
+from unittest.mock import patch
+from rc import RcSection
 
 # %% Testcases for rc.py
 
+
 class TestRC(unittest.TestCase):
+    """Unittest Testcase to test the rc.py module that contains
+    utility functions for reinforced concrete sections.
+    """
 
     def setUp(self):
-        self.width = 1000
+        """Initial definitions to set up subsequent tests.
+        """
+        self.width = 200
         self.thk = 1750
         self.rc = RcSection(self.width, self.thk)
+        self.rebar_od = 32
+        self.cover = 165
+        self.rebar_pos_y = self.thk - self.cover - self.rebar_od/2
+        self.rc.add_rebar(self.rebar_od, 0, self.rebar_pos_y)
 
     def test_simple(self):
+        """Test instantiation.
+        """
         self.assertEqual(type(self.rc), RcSection)
 
     def test_size_output(self):
+        """Test the get_extents() method of the RcSection class.
+        """
         test_output = (self.width, self.thk)
         self.assertEqual(test_output, self.rc.get_extents())
 
-    def test_plot(self):
+    @patch("rc.plt.show")
+    def test_plot(self, mock_show):
+        """Test the plotting method of the RcSection class.
+        """
+        mock_show.return_value = None
         fig, axis = self.rc.plot()
         self.assertNotEqual(fig.get_axes(), [])
+        self.assertEqual(len(axis.patches), len(self.rc.rebars)+1)
 
 #    def test_rebar(self):
 
 # %% Testcases for resp_spect.py
 
+
 class TestResponseSpectrum(unittest.TestCase):
     pass
+
 
 class TestBroadbanding(unittest.TestCase):
     pass
